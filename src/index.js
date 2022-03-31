7b93fcabea3a20d51002e720ea6967d8f553a9c8
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-31 12:02:36 +0200

    add script on html, and create function getJoke for get random joke

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@
  * Tout d'abord, je vais créer une fonction qui va me permettre de récupérer les données de l'API via la méthode fetch
  * Je vais ensuite afficher les données dans mon HTML
  */
+
+// Ici, je déclare une constante API qui est le endpoint de l'API
+const API = "https://api.chucknorris.io/jokes/random";
+
+// Je créer une fonction qui va me permettre de récupérer les données de l'API
+function getJoke() {
+	// J'utilise la méthode fetch pour récupérer les données de l'API
+	fetch(API)
+		// Je vais ensuite récupérer les données de l'API
+		.then((response) => response.json())
+		// je console.log() les données récupérées
+		.then((data) => {
+			console.log(data);
+		});
+}
+
+// j'appel ma fonction getJoke()
+getJoke();
