20b54bfe54a6e1401d07e569d7bec0abfd219ca3
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-03-31 11:48:50 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+/**
+ * Le but de ce livecoding, et de montrer comment je consomme une API en JS Vanilla
+ * Tout d'abord, je vais créer une fonction qui va me permettre de récupérer les données de l'API via la méthode fetch
+ * Je vais ensuite afficher les données dans mon HTML
+ */
